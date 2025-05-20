620bbd85db9fec4c038c11a91b75cc9989cfe8a8
BWOH <bwoh@outlook.kr>
2025-05-21 05:19:52 +0900

    ✨ Join 기능 추가

--- src/main/kotlin/kr/ac/kumoh/ce/s20250000/mysql_robot/RobotRepository.kt
+++ src/main/kotlin/kr/ac/kumoh/ce/s20250000/mysql_robot/RobotRepository.kt
@@ -1,8 +1,27 @@
 package kr.ac.kumoh.ce.s20250000.mysql_robot
 
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
 
 @Repository
 interface RobotRepository : JpaRepository<Robot, Int> {
+    @Query("""
+        SELECT new kr.ac.kumoh.ce.s20250000.mysql_robot.EquipmentDto(
+            m.name as robotName,
+            m.model,
+            m.robotClass,
+            e.location,
+            e.quantity,
+            w.name as weaponName,
+            w.type as weaponType,
+            w.damage,
+            w.ability
+        )
+        FROM Robot m
+        LEFT JOIN Equipment e ON m.id = e.mechanicId
+        LEFT JOIN Weapon w ON e.weaponId = w.id
+        ORDER BY m.name, e.location
+    """)
+    fun findAllEquipment(): List<EquipmentDto>
 }
